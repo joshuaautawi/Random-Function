23b7e23e818277964bd184a66b010b8643468296
joshuaautawi <autawijoshua@gmail.com>
2021-09-09 19:10:47 +0700

    new

---
+++ randomNumber.js
@@ -0,0 +1,13 @@
+
+function randomNumberFromTo (min,max){
+    return Math.floor(Math.random()*(max-min) +min)
+}
+
+
+function randomNumberFromZero (num){
+    return Math.floor(Math.random()*num)
+}
+
+
+
+console.log(randomNumber())
